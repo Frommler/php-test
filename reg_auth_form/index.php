91cf182a4e8506af4231eaa8e36db4413e0f1a81
Alex Vakula <albanec777@meta.ua>
2021-02-20 17:06:49 +0100

    PHP-auth-reg-form
    add index.php/register.php/connect.php/signup.php /main.css/

---
+++ reg_auth_form/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="assets/css/main.css ">
+  <title>Aurtherisation and Registration</title>
+</head>
+<body>
+  <!-- Authorisation form-->
+  <form class="authForm" action="" method="" >
+    <label for="">Login</label>
+    <input type="text" placeholder="Login">
+    <label for="">Password</label>
+    <input type="password" placeholder="password">
+    <button>Log in</button>
+  </form>
+</body>
+</html>
